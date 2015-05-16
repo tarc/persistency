c332ab1ff2e2ac2ee6629b2400fbde65139d566a
Tarcísio Rodrigues <tarcisio.genaro@gmail.com>
2015-05-16 17:50:58 -0300

    Add ut for trivial implementations.

--- context.hpp
+++ context.hpp
@@ -1,21 +1,22 @@
 namespace Persistency
 {
-	template< class Implementation >
-	class Context
-	{
-		public:
-			bool commit();
+  template< class Implementation >
+    class Context
+    {
+      public:
+        bool commit();
 
-		private:
-			Implementation * get_implementation()
-			{
-				return static_cast< Implementation * >( this );
-			}
-	};
+      private:
+        Implementation * get_implementation()
+        {
+          return static_cast< Implementation * >( this );
+        }
+    };
+
+  template< class Implementation >
+    bool Context< Implementation >::commit()
+    {
+      return get_implementation() -> _commit();
+    }
 }
 
-template< class Implementation >
-bool Persistency::Context< Implementation >::commit()
-{
-	return get_implementation() -> _commit();
-}
--- test/t01.cc
+++ test/t01.cc
@@ -2,25 +2,32 @@
 
 #include "context.hpp"
 
-class FalseImpl : public Persistency::Context< FalseImpl >
+template< bool trivial_value >
+class TrivialImplementation :
+  public Persistency :: Context<  TrivialImplementation< trivial_value >  >
 {
-	friend class Persistency::Context< FalseImpl >;
-	private:
-		bool _commit()
-		{
-			return false;
-		}
+  friend class Persistency ::
+    Context<  TrivialImplementation< trivial_value >  >;
+
+  private:
+  bool _commit()
+  {
+    return trivial_value;
+  }
 };
 
-TEST ( Include , Normal )
+TEST ( TrivialImplementation , Normal )
 {
-	FalseImpl false_impl;
-	EXPECT_EQ ( false_impl.commit(), false );
+  TrivialImplementation< true > true_impl;
+  EXPECT_EQ ( true_impl.commit(), true );
+
+  TrivialImplementation< false > false_impl;
+  EXPECT_EQ ( false_impl.commit(), false );
 }
 
 int main(int argc, char** argv)
 {
-	testing::InitGoogleTest(&argc, argv);
+  testing::InitGoogleTest(&argc, argv);
 
-	return RUN_ALL_TESTS();
+  return RUN_ALL_TESTS();
 }
